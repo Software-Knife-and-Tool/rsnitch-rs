cbcd81207635ea9bbdf73aa1449f56196b55d566
James M. Putnam <putnamjm.design@gmail.com>
2023-06-04 19:29:38 -0400

    #14 - Host::poll leaks file descriptors

--- src/snitch/mod.rs
+++ src/snitch/mod.rs
@@ -1,3 +1,6 @@
+//  SPDX-FileCopyrightText: Copyright 2023 James M. Putnam (putnamjm.design@gmail.com)
+//  SPDX-License-Identifier: MIT
+// mod button_builder;
 mod controls_box;
 mod group_box;
 mod host;
